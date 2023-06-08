743a06e8632e8aa526ea7c6ed12207554029b0b6
Tuan Duc Design <nguoisoidangyeu@outlook.com.vn>
2023-06-09 02:05:28 +0700

    Update package
    Move React to Preact

--- next.config.mjs
+++ next.config.mjs
@@ -15,10 +15,19 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-  experimental: {
-    scrollRestoration: true,
-  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  }
 }
 
 export default withMDX(nextConfig)
