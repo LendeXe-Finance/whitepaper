1b855ae5684d21799e03a425965400ce6c8fda12
Andrei27C <tech@andreicsatlos.dev>
2024-02-21 20:52:43 +0200

    hero pattern change

--- src/components/HeroPattern.jsx
+++ src/components/HeroPattern.jsx
@@ -4,16 +4,18 @@ export function HeroPattern() {
   return (
     <div className="absolute inset-0 -z-10 mx-0 max-w-none overflow-hidden">
       <div className="absolute left-1/2 top-0 ml-[-38rem] h-[25rem] w-[81.25rem] dark:[mask-image:linear-gradient(white,transparent)]">
-        <div className="absolute inset-0 bg-gradient-to-r from-[#36b49f] to-[#DBFF75] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#36b49f]/30 dark:to-[#DBFF75]/30 dark:opacity-100">
+        <div className="absolute inset-0 bg-gradient-to-r from-[#bf5434] to-[#ffcc00] opacity-40 [mask-image:radial-gradient(farthest-side_at_top,white,transparent)] dark:from-[#bf5434]/30 dark:to-[#ffcc00]/30 dark:opacity-100">
           <GridPattern
             width={72}
             height={56}
             x="-12"
             y="4"
             squares={[
-              [4, 3],
+              [3, 4],
               [2, 1],
-              [7, 3],
+              [5, 3],
+              [7, 4],
+              [8, 5],
               [10, 6],
             ]}
             className="absolute inset-x-0 inset-y-[-50%] h-[200%] w-full skew-y-[-18deg] fill-black/40 stroke-black/50 mix-blend-overlay dark:fill-white/2.5 dark:stroke-white/5"
